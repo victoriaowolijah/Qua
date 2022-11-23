c0b012f3b6aa886659ce23c54c688aadc8415569
victoria <victoriaowolijah@gmail.com>
2022-11-23 10:56:42 +0100

    commit

---
+++ quina/src/layouts/MainLayout.vue
@@ -0,0 +1,142 @@
+<template>
+  <q-layout view="hHh lpR lff">
+
+    <q-header bordered class="bg-primary text-black" height-hint="98">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          
+          KY
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="center">
+        <q-route-tab to="/Home" label="Home" />
+        <q-route-tab to="/About" label="About" />
+        <q-route-tab to="/Services" label="Services" />
+        <!-- <q-route-tab to="/page3" label="Page four" /> -->
+      </q-tabs>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <div class="q-mt-md q-mr-sm">Travel The World</div>
+
+  
+    <q-list>
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>USA</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>2 min ago</q-item-label>
+          <div class="text-orange">
+            <q-icon name="star" />
+            <q-icon name="star" />
+            <q-icon name="star" />
+          </div>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>UK</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>2 min ago</q-item-label>
+          <div class="text-orange">
+            <q-icon name="star" />
+            <q-icon name="star" />
+            <q-icon name="star" />
+          </div>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>CHINA</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>2 min ago</q-item-label>
+          <div class="text-orange">
+            <q-icon name="star" />
+            <q-icon name="star" />
+            <q-icon name="star" />
+          </div>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>AFRICA</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>2 min ago</q-item-label>
+          <div class="text-orange">
+            <q-icon name="star" />
+            <q-icon name="star" />
+            <q-icon name="star" />
+          </div>
+        </q-item-section>
+      </q-item>
+      
+
+    
+
+    
+    </q-list>
+  
+
+
+      <!-- <q-icon name="font_download" /> -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+$primary   : #46daa1;
+    <q-footer reveal bordered class="bg-#46daa1-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          
+          <div align="center">@Copyright 2022</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
---
+++ quina/src/pages/Index.vue
@@ -0,0 +1,93 @@
+<template>
+  <q-page class="flex flex-center col-4">
+    
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg"     style="height: 280px; max-width: 300px">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+      <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg"         style="height: 280px; max-width: 300px">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+      <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg"         style="height: 280px; max-width: 300px">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+      <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg"         style="height: 280px; max-width: 300px">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+      <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg"         style="height: 280px; max-width: 300px">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+    
+    <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg"     style="height: 280px; max-width: 300px">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+    </div>
+
+
+
+  </q-page>
+  
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
